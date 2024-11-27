c8a457a984b96dcae4079e3d46cd6456e29c353a
Glioh <mtl5475@psu.edu>
2024-11-26 23:17:03 -0500

    Sorted out logos and pictures

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ const App = () => {
   return (
    <BrowserRouter>
     <div className="relative z-0 bg-primary">
-      <div classNamee="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
         <Navbar />
         <Hero />
       </div>
       <About />
-      <Exerience />
+      <Experience />
       <Tech />
       <Works />
       <Feedbacks />
