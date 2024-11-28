952b25bc22ffbdad36fdada6c998ffbcacc0c685
Glioh <mtl5475@psu.edu>
2024-11-27 21:54:41 -0500

    Fixed the alignment issues with the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,25 +6,39 @@ import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
-  const [active, setActive] = useState('')
+  const [active, setActive] = useState('');
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      className={`w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+      <div className="flex items-center justify-between w-full px-8 lg:px-16 xl:px-24">
         <Link
-        to="/"
-        className="flex items-center gap-2"
-        onClick={() => {
-          setActive("");
-          window.scrollTo(0, 0)
-        }}
+          to="/"
+          className="flex items-center gap-3"
+          onClick={() => {
+            setActive('');
+            window.scrollTo(0, 0);
+          }}
         >
-          <img src={logo} alt="logo" className="w-0 h-9 object-contain" />
+          <img src={logo} alt="logo" className="w-10 h-10 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Michael Luu &nbsp;
+            <span className="sm:block hidden">| Software Engineer</span>
+          </p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className="text-white text-[16px] font-medium hover:text-secondary"
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
